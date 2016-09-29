843fc27734346a94d016f1f7032f38568a12eda1
deyouna5 <deyouna5@ATC-176-18>
2016-09-29 15:02:23 -0400

    Added Display Class

--- DMSE Project 201/Display.cs
+++ DMSE Project 201/Display.cs
@@ -4,14 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DMSE_Project_201
+namespace Seng201ProjectOne
 {
     class Display
     {
-        public void DisplayInfo(string test)
+        int counter = 1;
+        int input;
+
+        public void DisplayNames(ref Student student)
         {
-            Console.WriteLine(test);
-            Console.WriteLine(test);
+            Console.WriteLine("{0}) {1} {2}", counter, student.FirstName, student.LastName);
+            counter++;
+        }
+
+        public int PromptUser()
+        {
+            Console.Write("\nChoose a students information to view: ");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            return input;
+        }
+
+        public void DisplayStudentInfo(ref Student student)
+        {
+            Console.WriteLine("\nStudentID: {0}", student.Id);
+            Console.WriteLine("StudentFirstName: {0}", student.FirstName);
+            Console.WriteLine("StudentLastName: {0}", student.LastName);
+            Console.WriteLine("CourseID: {0}", student.CourseId);
+            Console.WriteLine("CourseNumber: {0}", student.CourseNumber);
+            Console.WriteLine("CourseName: {0}", student.CourseName);
+            Console.WriteLine("Credit: {0}", student.Credits);
+            Console.WriteLine("Semester: {0}", student.Semester);
+            Console.WriteLine("Year: {0}", student.Year);
+            Console.WriteLine("CourseType: {0}", student.CourseType);
+            Console.WriteLine("CourseGrade: {0}", student.CourseGrade);
         }
     }
 }
+
