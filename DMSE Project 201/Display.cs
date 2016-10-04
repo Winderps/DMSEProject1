7cbedb4a18cec24c18e762cb72c70dc5a6562ec5
sousaa <sousaa@ATC-176-15>
2016-10-04 14:11:07 -0400

    Added More Display Methods

--- DMSE Project 201/Display.cs
+++ DMSE Project 201/Display.cs
@@ -9,23 +9,49 @@ namespace DMSE_Project_201
     class Display
     {
         int counter = 1;
-        int input;
+        int inputStudent;
+        int inputCourse;
 
-        public void DisplayNames(ref Student student)
+        public void DisplayNames(Array students)
         {
-            Console.WriteLine("{0}) {1} {2}", counter, student.FirstName, student.LastName);
-            counter++;
+            foreach (Student student in students)
+            {
+                Console.WriteLine("{0}) {1} {2}", counter, student.FirstName, student.LastName);
+                counter++;
+            }
         }
 
-        public int PromptUser()
+        public int PromptUserStudents()
         {
             Console.Write("\nChoose a students information to view: ");
-            input = Convert.ToInt32(Console.ReadLine());
+            inputStudent = Convert.ToInt32(Console.ReadLine());
 
-            return input;
+            return inputStudent;
         }
 
-        public static void DisplayStudentInfo(ref Student student)
+        public void DisplayCourses(Student student)  //Method call (*Array: Student[input])
+        {
+            foreach (Course course in student.Courses)
+            {
+                Console.WriteLine("{0}) {1}", counter, course.Name);
+                counter++;
+            }
+        }
+
+        public int PromptUserCourses()
+        {
+            Console.WriteLine("\nChoose which course you would like to view: ");
+            inputCourse = Convert.ToInt32(Console.ReadLine());
+
+            return inputCourse;
+        }
+        
+           static public Course GetCourse(List<Course> CourseList, int course)
+        {
+            return CourseList[course];
+        }
+
+        public static void DisplayStudentInfo(ref Student student, ref Course course)
         {
             Console.WriteLine("\nStudentID: {0}", student.ID);
             Console.WriteLine("StudentFirstName: {0}", student.FirstName);
@@ -39,6 +65,7 @@ namespace DMSE_Project_201
             Console.WriteLine("CourseType: {0}", student.CourseType);
             Console.WriteLine("CourseGrade: {0}", student.CourseGrade);*/
         }
+     
     }
 }
 
